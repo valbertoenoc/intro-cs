ac51a0b08fe1aca3730a048ad4700925efcd6eed
valbertoenoc <valbertoenoc@gmail.com>
2019-03-05 10:10:03 -0300

    typo fix

--- week2-intro/ex2_media.py
+++ week2-intro/ex2_media.py
@@ -1,6 +1,6 @@
-nota1 = int(input('Digite a primeira nota: '))4
-nota2 = int(input('Digite a segunda nota: '))4
-nota3 = int(input('Digite a terceira nota: '))4
-nota4 = int(input('Digite a quarta nota: '))4
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+nota3 = int(input('Digite a terceira nota: '))
+nota4 = int(input('Digite a quarta nota: '))
 
 print('A média aritmética é {}'.format((nota1 + nota2 + nota3 + nota4)/4))
